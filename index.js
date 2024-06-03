e114aa77f15d4fe4126c3f6f1e8c57b3a9c9108a
Nitinsegall <nitinsehgal384@gmail.com>
2024-06-04 01:47:38 +0530

    make the website more responsive

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let cvbtn=document.querySelector("#cvBtn");
 let submit=document.querySelector("#Submit");
+let bar=document.querySelector("#bar");
+
 cvbtn.addEventListener("click",Click=()=>{
     alert("Download Successfully")
 })
@@ -9,11 +11,24 @@ cvbtn.addEventListener("click",Click=()=>{
 submit.addEventListener("click",form=()=>{
     let email=document.querySelector("#Email");
     let msg=document.querySelector("#Message");
-
-if(email.value==""&& msg.value==""){
-    alert("* fields are Required");
-}else{
-   alert("logged in")
-}
+    
+    if(email.value==""&& msg.value==""){
+        alert("* fields are Required");
+    }else{
+        alert("logged in")
+    }
 
 })
+
+    let ul=document.querySelector('ul')
+
+bar.addEventListener("click",Click=()=>{
+        ul.classList.toggle('showData')
+        if(ul.className=='showData'){
+            bar.className='fa-solid fa-xmark'
+          
+        }else{
+            bar.className='fa-solid fa-bars'
+
+        }
+    })
